96a43eccbd4d72a65377842c1a1bb165f8efadef
elyk8 <kylese58@gmail.com>
2022-06-03 08:32:53 +1000

    a new emacs configuration

---
+++ early-init.el
@@ -0,0 +1,53 @@
+; -*- lexical-binding: t; -*-
+
+;; Garbage Collections
+(setq gc-cons-percentage 0.6)
+
+;; Compile warnings
+;;  (setq warning-minimum-level :emergency)
+(setq native-comp-async-report-warnings-errors nil) ;; native-comp warning
+(setq byte-compile-warnings '(not free-vars unresolved noruntime lexical make-local))
+
+
+;; MISC OPTIMIZATIONS ----
+;;; optimizations (froom Doom's core.el). See that file for descriptions.
+(setq idle-update-delay 1.0)
+
+;; Disabling bidi (bidirectional editing stuff)
+(setq-default bidi-display-reordering 'left-to-right
+              bidi-paragraph-direction 'left-to-right)
+(setq bidi-inhibit-bpa t)  ; emacs 27 only - disables bidirectional parenthesis
+
+(setq-default cursor-in-non-selected-windows nil)
+(setq highlight-nonselected-windows nil)
+(setq fast-but-imprecise-scrolling t)
+(setq inhibit-compacting-font-caches t)
+
+;; Don't pop up UI dialogs when prompting
+(setq use-dialog-box nil)
+
+(when (fboundp 'startup-redirect-eln-cache)
+  (startup-redirect-eln-cache
+   (convert-standard-filename
+	  (expand-file-name  "var/eln-cache/" user-emacs-directory))))
+
+;; Escape exit everything
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+;; For evil mode
+(global-set-key (kbd "C-M-u") 'universal-argument)
+
+;; Window configuration
+(setq frame-inhibit-implied-resize t) ;; Supposed to hasten startup
+
+;; Less clutter (this is what dfrosted12 uses so I trust that)
+(add-to-list 'default-frame-alist '(tool-bar-lines . 0))
+(add-to-list 'default-frame-alist '(menu-bar-lines . 0))
+(add-to-list 'default-frame-alist '(vertical-scroll-bars))
+
+;; This makes the Aqua titlebar color the same as Emacs.
+(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+
+;; This makes emacs transparent
+(set-frame-parameter (selected-frame) 'alpha '(95 . 95))
+(add-to-list 'default-frame-alist '(alpha . (95 . 95)))
